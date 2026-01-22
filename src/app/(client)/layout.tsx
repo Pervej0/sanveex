057ea8a7698804484e3b9ace2f0eda3c5648fc33
Pervej0 <mdparvez222khan@gmail.com>
2026-01-22 23:40:06 +0600

    clientLayout updated

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -2,25 +2,31 @@ import Footer from "@/components/shared/Footer";
 import Navbar from "@/components/shared/Navbar";
 import Topbar from "@/components/shared/Topbar";
 import WhatsAppButton from "@/components/shared/WhatsAppButton";
+import prisma from "@/lib/prisma";
 
 export default async function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/site-content`,
-  );
+  const siteContent = await prisma.siteContent.findFirst();
 
-  const siteContent = await res.json();
+  if (!siteContent) {
+    return (
+      <div>
+        <h1>History</h1>
+        <p>Content not available.</p>
+      </div>
+    );
+  }
 
   return (
     <>
-      <Topbar siteContent={siteContent.data} />
-      <Navbar siteContent={siteContent.data} />
+      <Topbar siteContent={siteContent} />
+      <Navbar siteContent={siteContent} />
       {children}
-      <Footer siteContent={siteContent.data} />
-      <WhatsAppButton siteContent={siteContent.data} />
+      <Footer siteContent={siteContent} />
+      <WhatsAppButton siteContent={siteContent} />
     </>
   );
 }
